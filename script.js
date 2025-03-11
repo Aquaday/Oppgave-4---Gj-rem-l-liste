3310a4a42e0d30a9c0c39c3b7c4f417993a082b9
Aquaday <martin991940@hotmail.com>
2025-03-11 11:34:30 +0100

    start oppgave

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+let lagreButtonEl = document.querySelector("#lagreButton")
+let gjoremalListeEl = document.querySelector("#gjormalListe")
+
+
+
+document.addEventListener("click", () => {
+    let overskrift = document.querySelector("#overskrift").value;
+    let langreInfo = document.querySelector("#lengreInfo").value;
+    let viktighet = document.querySelector("#viktighet").value;
+
+    gjoremalListe.createElement("h1")
+    gjoremalListe.createElement("p")
+    gjoremalListe.createElement("h4")
+
+})
