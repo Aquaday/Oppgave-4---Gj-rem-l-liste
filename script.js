6f2b8f08c87d715d20fb1ed88b84c5240c3145ad
Aquaday <martin991940@hotmail.com>
2025-03-12 12:30:15 +0100

    .

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let viktighetArray = []
 
 
 function getLocal() { 
-    const savedOverskriftArray = localStorage.getItem("overskrift")
-    const savedInfoArray = localStorage.getItem("info")
-    const savedViktighetArray = localStorage.getItem("viktighet")
+    let savedOverskriftArray = localStorage.getItem("overskrift")
+    let savedInfoArray = localStorage.getItem("info")
+    let savedViktighetArray = localStorage.getItem("viktighet")
 
     let overskriftArrayNy = JSON.parse(savedOverskriftArray)
     let infoArrayNy = JSON.parse(savedInfoArray)
@@ -30,7 +30,6 @@ function getLocal() {
     }
 }
 
-
 if (localStorage.getItem("overskrift") === null){
     console.log("Hello, first time visitor")
 } else {
@@ -38,72 +37,64 @@ getLocal()
 console.log("Welcome back!")
 }
 
+function addFormToArray() {
+        let overskrift = document.querySelector("#overskrift").value;
+        let info = document.querySelector("#info").value;
+        let viktighet = document.querySelector("#viktighet").value;
+
+        overskriftArray.push(overskrift)
+        infoArray.push(info)
+        viktighetArray.push(viktighet)
+
+        let stringifiedOverskrift = JSON.stringify(overskriftArray)
+        let stringifiedInfo = JSON.stringify(infoArray)
+        let stringifiedViktighet = JSON.stringify(viktighetArray)
+        
+        localStorage.setItem("overskrift", stringifiedOverskrift)
+        localStorage.setItem("info", stringifiedInfo)
+        localStorage.setItem("viktighet", stringifiedViktighet)
+
+        let nyOverskrift = document.createElement("h1")
+        let nyInfo = document.createElement("p")
+        let nyViktighet = document.createElement("h4")
+
+        nyOverskrift.textContent = overskrift
+        nyInfo.textContent = info
+        nyViktighet.textContent = viktighet
+
+        gjoremalListeEl.appendChild(nyOverskrift)
+        gjoremalListeEl.appendChild(nyInfo)
+        gjoremalListeEl.appendChild(nyViktighet)
+}
+
 lagreButtonEl.addEventListener("click", () => {
+    if (localStorage.getItem("overskrift") === null){
+        addFormToArray()
+    } else {
     let savedOverskriftArray = localStorage.getItem("overskrift")
     let savedInfoArray = localStorage.getItem("info")
     let savedViktighetArray = localStorage.getItem("viktighet")
 
-    let overskriftArray = JSON.parse(savedOverskriftArray)
-    let infoArray = JSON.parse(savedInfoArray)
-    let viktighetArray = JSON.parse(savedViktighetArray)
+    overskriftArray = JSON.parse(savedOverskriftArray)
+    infoArray = JSON.parse(savedInfoArray)
+    viktighetArray = JSON.parse(savedViktighetArray)
 
-    let overskrift = document.querySelector("#overskrift").value;
-    let info = document.querySelector("#info").value;
-    let viktighet = document.querySelector("#viktighet").value;
-
-    overskriftArray.push(overskrift)
-    infoArray.push(info)
-    viktighetArray.push(viktighet)
-
-    let stringifiedOverskrift = JSON.stringify(overskriftArray)
-    let stringifiedInfo = JSON.stringify(infoArray)
-    let stringifiedViktighet = JSON.stringify(viktighetArray)
-    
-    localStorage.setItem("overskrift", stringifiedOverskrift)
-    localStorage.setItem("info", stringifiedInfo)
-    localStorage.setItem("viktighet", stringifiedViktighet)
-
-    let nyOverskrift = document.createElement("h1")
-    let nyInfo = document.createElement("p")
-    let nyViktighet = document.createElement("h4")
-
-    nyOverskrift.textContent = overskrift
-    nyInfo.textContent = info
-    nyViktighet.textContent = viktighet
-
-    gjoremalListeEl.appendChild(nyOverskrift)
-    gjoremalListeEl.appendChild(nyInfo)
-    gjoremalListeEl.appendChild(nyViktighet)
-
-    
-
+    addFormToArray()
+    }
 })
 
 function clearGjoremal() {
-    localStorage.removeItem("overskrift")
-    localStorage.removeItem("info")
-    localStorage.removeItem("viktighet")
+    localStorage.removeItem("overskrift", "info", "viktighet")
     location.reload()
 }
 
 document.querySelector("#clearForm").addEventListener("click", clearGjoremal)
 
-// if (overskriftArray.length === 0) {
-//     console.log("hello")
-// } else {
-// for (let i = 0; i < overskriftArray.length; i++) {
-//     let nyOverskrift = document.createElement("h1")
-//     let nyInfo = document.createElement("p")
-//     let nyViktighet = document.createElement("h4")
+// let fjernGjoremalKnapp = document.querySelector("#fjernGjoremal")
 
-//     nyOverskrift.textContent = overskriftArray[i]
-//     nyInfo.textContent = infoArray[i]
-//     nyViktighet.textContent = viktighetArray[i]
+// function ferdigGjoremal() {
+    
+// }
 
-//     gjoremalListeEl.appendChild(nyOverskrift)
-//     gjoremalListeEl.appendChild(nyInfo)
-//     gjoremalListeEl.appendChild(nyViktighet)
-
-// }}
-
+// fjernGjoremalKnapp.addEventListener("click", ferdigGjoremal)
 
